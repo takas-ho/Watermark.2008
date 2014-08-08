d6b228cd818c2a6d65b14ec71e3f367a64729c1e
HOMMA Takashi <hommatks@gmail.com>
2014-08-08 09:51:23 +0900

    Watermark TextBox を追加する

--- Watermark.2008/Form1.Designer.vb
+++ Watermark.2008/Form1.Designer.vb
@@ -22,9 +22,30 @@ Partial Class Form1
     'コード エディタを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Watermark1 = New Watermark
+        Me.SuspendLayout()
+        '
+        'Watermark1
+        '
+        Me.Watermark1.Location = New System.Drawing.Point(6, 14)
+        Me.Watermark1.Name = "Watermark1"
+        Me.Watermark1.Size = New System.Drawing.Size(458, 19)
+        Me.Watermark1.TabIndex = 0
+        Me.Watermark1.WatermarkColor = System.Drawing.Color.Gray
+        Me.Watermark1.WatermarkText = "Default Watermark"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(470, 266)
+        Me.Controls.Add(Me.Watermark1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Watermark1 As Watermark
 
 End Class
